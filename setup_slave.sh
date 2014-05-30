a1869e5fd0b232b4242a0b6ec52ed53f8b4ad2d6
Amy Unruh <amy@infosleuth.net>
2014-05-30 13:52:41 -0700

    support static ip for master, fixed slave config

--- setup_slave.sh
+++ setup_slave.sh
@@ -4,6 +4,7 @@ source $UTIL_INSTALL_DIR/pathfile
 
 REDIS_MASTER=`gcloud preview resource-views --zone=$ZONE resources --resourceview=$VIEW_NAME list -l | python $UTIL_INSTALL_DIR/getInstanceName.py `
 
+echo masterauth $REDIS_AUTH >> /etc/redis/6379.conf
 echo slaveof $REDIS_MASTER 6379 >> /etc/redis/6379.conf
 /etc/init.d/redis_6379 start
 /etc/init.d/redis_26379 start
